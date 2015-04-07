b08aea145aeecb8e4e8d0519cd9e8a600977d2de
Mathias Buus <mathiasbuus@gmail.com>
2015-04-07 10:13:37 -0700

    support inner and outer scope using a generator loop. fixes #4 and #2

--- repl.js
+++ repl.js
@@ -2,18 +2,29 @@
   var repl = require('repl')
   var os = require('os')
   var empty = '(' + os.EOL + ')'
+
+  var sources = []
+  var loop = function* (){
+    while (true) {
+      if (sources.length) {
+        var next = sources.shift()
+        try {
+          next.callback(null, eval(next.command))
+        } catch (e) {
+          next.callback(e)
+        }
+      }
+      yield null
+    }
+  }()
+
   repl.start({
     input: process.stdin,
     output: process.stdout,
     eval: function(cmd, context, filename, callback) {
       if (cmd === empty) return callback()
-      try {
-        var result = eval.call(self, cmd)
-        callback(null, result)
-      }
-      catch (e) {
-        callback(e)
-      }
+      sources.push({command: cmd, callback: callback})
+      loop.next()
     }
   })
 })(this);
