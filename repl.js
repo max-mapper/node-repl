0b6e73147ed13839f164af717b8c428e0a140ba3
Mathias Buus <mathiasbuus@gmail.com>
2015-04-07 16:37:32 -0700

    changed namespacing and standard style

--- repl.js
+++ repl.js
@@ -1,18 +1,18 @@
-;(function() { // __var to avoid clashes
-  var __sources = []
-  var __loop = function* (){
+;(function () { // uppercase namespaces var to avoid clashes
+  var NODE_REPL_SOURCES = []
+  var NODE_REPL_LOOP = (function * () {
     while (true) {
-      if (__sources.length) {
-        var __next = __sources.shift()
+      if (NODE_REPL_SOURCES.length) {
+        var NODE_REPL_NEXT = NODE_REPL_SOURCES.shift()
         try {
-          __next.callback(null, eval(__next.command))
+          NODE_REPL_NEXT.callback(null, eval(NODE_REPL_NEXT.command))
         } catch (e) {
-          __next.callback(e)
+          NODE_REPL_NEXT.callback(e)
         }
       }
       yield null
     }
-  }()
+  })()
 
   ;(function () {
     var repl = require('repl')
@@ -22,11 +22,11 @@
     repl.start({
       input: process.stdin,
       output: process.stdout,
-      eval: function(cmd, context, filename, callback) {
+      eval: function (cmd, context, filename, callback) {
         if (cmd === empty) return callback()
-        __sources.push({command: cmd, callback: callback})
-        __loop.next()
+        NODE_REPL_SOURCES.push({command: cmd, callback: callback})
+        NODE_REPL_LOOP.next()
       }
     })
   })()
-})();
+})()
